9b1b5fb2626901774b666225646c6495c3f59a0f
aysieelf <aysie.elf@gmail.com>
2024-07-03 17:43:53 +0300

    Create above_diagonal.py

---
+++ Week-1/2-Multidimensional_lists/above_diagonal.py
@@ -0,0 +1,13 @@
+# N for how many rows and columns our 2d list has
+n = int(input())
+
+# creates a 2d list
+a = [[2**(i+j) for j in range(n)] for i in range(n)]
+
+# summarising only above the diagonal
+ttl = 0
+for index in range(n):
+    ttl += sum(a[index][index+1:])
+
+# printing the total sum
+print(ttl)
