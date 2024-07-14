d0c055af2a01f8539399a62e0f1a4656ee6c1079
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2024-07-14 15:34:54 +0300

    updating

--- ex.py
+++ ex.py
@@ -1,22 +1,12 @@
-# the_dict = {'Steven': 21, 'Alice': 23, 'John': 18}
-#
-# print(the_dict.items())
-# val_sort = sorted(the_dict.items(), key=lambda x: x[1])
-# print(val_sort)
-# #
+class Product:
+    def __init__(self, name: str, price: float):
+        self.name = name
+        self.price = price
 
-# lst_values = sorted(the_dict.values())
-# sorted_dict = {}
-#
-# while lst_values:
-#     for key, value in the_dict.items():
-#         if not lst_values:
-#             break
-#         if value == lst_values[0]:
-#             sorted_dict[key] = value
-#             lst_values.remove(lst_values[0])
-#
-# sorted_list = [(k, v) for k, v in sorted_dict.items()]
-#
-# print(sorted_dict)
-# print(sorted_list)
+
+class ShoppingCart:
+    def __init__(self):
+        self.items: list[Product] = []
+
+    def add_product(self, product: Product):
+        pass
